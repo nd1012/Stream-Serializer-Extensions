6610fe40fdcd5ed5c22fa85da296f7394c6a1d8e
nd <nd1012@users.noreply.github.com>
2023-04-22 09:11:05 +0700

    Dev (#2)
    +Long length validation helper method
    + Added ObjectValidator library reference
    + `StreamSerializerBase` extends `ValidatableObject`
    + `StreamSerializerAdapter` for easy `IStreamSerializerVersion` implementation
    - Fixed enumeration value validation (failed with flags)

---
+++ src/Stream-Serializer-Extensions/StreamSerializerAdapter.cs
@@ -0,0 +1,39 @@
+﻿using wan24.Core;
+
+namespace wan24.StreamSerializerExtensions
+{
+    /// <summary>
+    /// Stream serializer adapter methods for simple implementation of <see cref="IStreamSerializerVersion"/>
+    /// </summary>
+    public static class StreamSerializerAdapter
+    {
+        /// <summary>
+        /// Read the serialized object version
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="serializerVersion">Serializer version</param>
+        /// <param name="objectVersion">Object version</param>
+        /// <returns>Serialized object version</returns>
+        public static int ReadSerializedObjectVersion(Stream stream, int serializerVersion, int objectVersion)
+        {
+            int res = stream.ReadNumber<int>(serializerVersion);
+            if (res > objectVersion) throw new SerializerException($"Unsupported object version {res} (max. supported version is {objectVersion})");
+            return res;
+        }
+
+        /// <summary>
+        /// Read the serialized object version
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="serializerVersion">Serializer version</param>
+        /// <param name="objectVersion">Object version</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Serialized object version</returns>
+        public static async Task<int> ReadSerializedObjectVersionAsync(Stream stream, int serializerVersion, int objectVersion, CancellationToken cancellationToken = default)
+        {
+            int res = await stream.ReadNumberAsync<int>(serializerVersion, cancellationToken: cancellationToken).DynamicContext();
+            if (res > objectVersion) throw new SerializerException($"Unsupported object version {res} (max. supported version is {objectVersion})");
+            return res;
+        }
+    }
+}
