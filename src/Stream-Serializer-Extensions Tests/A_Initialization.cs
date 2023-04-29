0094b391d894c7fc189879ee8fb42de2c78fb06c
nd <nd1012@users.noreply.github.com>
2023-04-29 10:23:51 +0200

    Create A_Initialization.cs

---
+++ src/Stream-Serializer-Extensions Tests/A_Initialization.cs
@@ -0,0 +1,13 @@
+﻿using wan24.ObjectValidation;
+
+namespace Stream_Serializer_Extensions_Tests
+{
+    [TestClass]
+    public class A_Initialization
+    {
+        public A_Initialization() => ValidateObject.Logger = (message) => Console.WriteLine(message);
+
+        [TestMethod]
+        public void Logger_Test() => ValidateObject.Logger("Stream-Serializer-Tests initialized");
+    }
+}
