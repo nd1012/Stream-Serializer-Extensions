2a7a23574c53dd666bca340ec43647c27bedcc3d
nd <nd1012@users.noreply.github.com>
2023-04-30 19:35:13 +0200

    Update
    + Added `DisposableStreamSerializerBase`

---
+++ src/Stream-Serializer-Extensions/DisposableStreamSerializerBase.cs
@@ -0,0 +1,136 @@
+﻿using wan24.Core;
+
+namespace wan24.StreamSerializerExtensions
+{
+    /// <summary>
+    /// Base class for a disposable stream serializing type
+    /// </summary>
+    public abstract class DisposableStreamSerializerBase : DisposableBase, IStreamSerializerVersion
+    {
+        /// <summary>
+        /// Object version
+        /// </summary>
+        private readonly int? _ObjectVersion;
+        /// <summary>
+        /// Serialized object version
+        /// </summary>
+        private int? _SerializedObjectVersion = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="objectVersion">Object version</param>
+        protected DisposableStreamSerializerBase(int? objectVersion = null) : base() => _ObjectVersion = objectVersion;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="version">Serializer version</param>
+        /// <param name="objectVersion">Object version</param>
+        protected DisposableStreamSerializerBase(Stream stream, int version, int? objectVersion = null) : base()
+        {
+            _ObjectVersion = objectVersion;
+            Deserialize(stream, version);
+        }
+
+        /// <inheritdoc/>
+        int? IStreamSerializerVersion.ObjectVersion => _ObjectVersion;
+
+        /// <inheritdoc/>
+        int? IStreamSerializerVersion.SerializedObjectVersion => _SerializedObjectVersion;
+
+        /// <summary>
+        /// Serialize
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        protected abstract void Serialize(Stream stream);
+
+        /// <summary>
+        /// Serialize
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        protected virtual async Task SerializeAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            await Task.Yield();
+            Serialize(stream);
+        }
+
+        /// <summary>
+        /// Serialize
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        private void SerializeInt(Stream stream)
+        {
+            if (_ObjectVersion != null) stream.WriteNumber(_ObjectVersion.Value);
+            Serialize(stream);
+        }
+
+        /// <summary>
+        /// Serialize
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        private async Task SerializeIntAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            if (_ObjectVersion != null) await stream.WriteNumberAsync(_ObjectVersion.Value, cancellationToken).DynamicContext();
+            await SerializeAsync(stream, cancellationToken).DynamicContext();
+        }
+
+        /// <summary>
+        /// Deserialize
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="version">Serializer version</param>
+        protected abstract void Deserialize(Stream stream, int version);
+
+        /// <summary>
+        /// Deserialize
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="version">Serializer version</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        protected virtual async Task DeserializeAsync(Stream stream, int version, CancellationToken cancellationToken)
+        {
+            await Task.Yield();
+            Deserialize(stream, version);
+        }
+
+        /// <summary>
+        /// Deserialize
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="version">Serializer version</param>
+        private void DeserializeInt(Stream stream, int version)
+        {
+            if (_ObjectVersion != null) _SerializedObjectVersion = StreamSerializerAdapter.ReadSerializedObjectVersion(stream, version, _ObjectVersion.Value);
+            Deserialize(stream, version);
+        }
+
+        /// <summary>
+        /// Deserialize
+        /// </summary>
+        /// <param name="stream">Stream</param>
+        /// <param name="version">Serializer version</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        private async Task DeserializeIntAsync(Stream stream, int version, CancellationToken cancellationToken)
+        {
+            if (_ObjectVersion != null)
+                _SerializedObjectVersion = await StreamSerializerAdapter.ReadSerializedObjectVersionAsync(stream, version, _ObjectVersion.Value, cancellationToken).DynamicContext();
+            await DeserializeAsync(stream, version, cancellationToken).DynamicContext();
+        }
+
+        /// <inheritdoc/>
+        void IStreamSerializer.Serialize(Stream stream) => SerializeInt(stream);
+
+        /// <inheritdoc/>
+        Task IStreamSerializer.SerializeAsync(Stream stream, CancellationToken cancellationToken) => SerializeIntAsync(stream, cancellationToken);
+
+        /// <inheritdoc/>
+        void IStreamSerializer.Deserialize(Stream stream, int version) => DeserializeInt(stream, version);
+
+        /// <inheritdoc/>
+        Task IStreamSerializer.DeserializeAsync(Stream stream, int version, CancellationToken cancellationToken) => DeserializeIntAsync(stream, version, cancellationToken);
+    }
+}
